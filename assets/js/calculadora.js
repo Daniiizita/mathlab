a14e9d754f75af07abda03278cd2d8cc0347596c
Danielle <goncalvesdani54@hotmail.com>
2023-07-03 11:03:09 -0300

    atualizacao 03-07-23

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,94 +1,33 @@
-class Calculator {
-    constructor(displayElement) {
-      this.displayElement = displayElement;
-      this.clear();
+let display = document.getElementById('display');
+    let currentExpression = '';
+
+    function appendNumber(number) {
+      currentExpression += number;
+      display.value = currentExpression;
     }
-  
-    clear() {
-      this.currentOperand = '';
-      this.previousOperand = '';
-      this.operation = undefined;
+
+    function appendOperator(operator) {
+      currentExpression += operator;
+      display.value = currentExpression;
     }
-  
-    delete() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1);
-    }
-  
-    appendNumber(number) {
-      if (number === '.' && this.currentOperand.includes('.')) return;
-      this.currentOperand = this.currentOperand.toString() + number.toString();
-    }
-  
-    chooseOperation(operation) {
-      if (this.currentOperand === '') return;
-      if (this.previousOperand !== '') {
-        this.compute();
+
+    function calculate() {
+      try {
+        let result = eval(currentExpression);
+        display.value = result;
+        currentExpression = result.toString();
+      } catch (error) {
+        display.value = 'Erro';
+        currentExpression = '';
       }
-      this.operation = operation;
-      this.previousOperand = this.currentOperand;
-      this.currentOperand = '';
     }
-  
-    compute() {
-      let computation;
-      const prev = parseFloat(this.previousOperand);
-      const current = parseFloat(this.currentOperand);
-      if (isNaN(prev) || isNaN(current)) return;
-      switch (this.operation) {
-        case '+':
-          computation = prev + current;
-          break;
-        case '-':
-          computation = prev - current;
-          break;
-        case '*':
-          computation = prev * current;
-          break;
-        case '/':
-          computation = prev / current;
-          break;
-        default:
-          return;
-      }
-      this.currentOperand = computation;
-      this.operation = undefined;
-      this.previousOperand = '';
+
+    function clearDisplay() {
+      display.value = '';
+      currentExpression = '';
     }
-  
-    updateDisplay() {
-      this.displayElement.value = this.currentOperand;
+
+    function deleteLast() {
+      currentExpression = currentExpression.slice(0, -1);
+      display.value = currentExpression;
     }
-  }
-  
-  const displayElement = document.getElementById('display');
-  const numberButtons = document.querySelectorAll('.number');
-  const operatorButtons = document.querySelectorAll('.operator');
-  const clearButton = document.querySelector('.clear');
-  const equalButton = document.querySelector('.equal');
-  
-  const calculator = new Calculator(displayElement);
-  
-  numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.appendNumber(button.innerText);
-      calculator.updateDisplay();
-    });
-  });
-  
-  operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      calculator.chooseOperation(button.innerText);
-      calculator.updateDisplay();
-    });
-  });
-  
-  clearButton.addEventListener('click', () => {
-    calculator.clear();
-    calculator.updateDisplay();
-  });
-  
-  equalButton.addEventListener('click', () => {
-    calculator.compute();
-    calculator.updateDisplay();
-  });
-  
